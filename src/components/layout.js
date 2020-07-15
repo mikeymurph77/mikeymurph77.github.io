cbfaeb4198d7580c2dd2d15294685b961de75a75
Michael Murphy <mmurphy@jobcase.com>
2020-07-14 20:36:14 -0400

    condensed nav implementation

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,48 @@
-import React from "react"
+import React, { Fragment } from "react"
 import { Link } from "gatsby"
 
 import { rhythm, scale } from "../utils/typography"
+import styled from "styled-components"
+
+const NavList = styled.ul`
+  text-align: center;
+
+  @media screen and (min-width: 601px) {
+    display: flex;
+    text-align: left;
+    justify-content: flex-end;
+    align-items: center;
+  }
+`;
+
+const Logo = styled.li`
+  list-style-type: none;
+
+  @media screen and (min-width: 601px) {
+    flex: 1;
+  }
+`;
+
+const NavItem = styled.li`
+  list-style-type: none;
+  @media screen and (min-width: 601px) {
+    margin-left: 15px;
+  }
+`;
+
+const HeaderNavLinks = styled.div`
+  display: inline-block;
+`;
+
+const StyledLink = styled(Link)`
+  test-decoration: none;
+  color: #fff;
+`;
+
+const Footer = styled.footer`
+  margin-top: ${rhythm(1)};
+  text-align: center;
+`;
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
@@ -9,42 +50,21 @@ const Layout = ({ location, title, children }) => {
 
   if (location.pathname === rootPath) {
     header = (
-      <h1
-        style={{
-          ...scale(1.5),
-          marginBottom: rhythm(1.5),
-          marginTop: 0,
-        }}
-      >
-        <Link
-          style={{
-            boxShadow: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title}
-        </Link>
-      </h1>
+      <img src="https://res.cloudinary.com/dtghgjg3i/image/upload/v1594746304/gatsby-personal-site/mike-murphy-logo-avatar-1_uoc8ax.png" alt="Mike Murphy" />
     )
   } else {
     header = (
-      <h3
-        style={{
-          fontFamily: `Montserrat, sans-serif`,
-          marginTop: 0,
-        }}
-      >
-        <Link
-          style={{
-            boxShadow: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title}
-        </Link>
-      </h3>
+      <nav>
+        <NavList>
+          <Logo>
+            <StyledLink to={`/`}><img src="https://res.cloudinary.com/dtghgjg3i/image/upload/v1594762701/gatsby-personal-site/mike-murphy-logo-thin-mobile_t0duep.png" alt="Mike Murphy" /></StyledLink>
+          </Logo>
+          <NavItem><Link to={`/`}>About</Link></NavItem>
+          <NavItem><Link to={`/blog`}>Blog</Link></NavItem>
+          <NavItem><Link to={`/resume`}>Resume</Link></NavItem>
+          <NavItem><Link to={`/portfolio`}>Portfolio</Link></NavItem>
+        </NavList>
+      </nav>
     )
   }
   return (
@@ -58,9 +78,16 @@ const Layout = ({ location, title, children }) => {
     >
       <header>{header}</header>
       <main>{children}</main>
-      <footer>
+      <Footer>
+        <div>
+          <a href='https://twitter.com/mikeymurph77' alt='twitter'>Twitter</a>
+          {` | `}
+          <a href='https://www.linkedin.com/in/michaelmmurphy/' alt='twitter'>LinkedIn</a>
+          {` | `}
+          <a href='mailto:mikemurphy@hey.com' alt='twitter'>Email</a>
+        </div>
         © {new Date().getFullYear()}, Mike Murphy
-      </footer>
+      </Footer>
     </div>
   )
 }
